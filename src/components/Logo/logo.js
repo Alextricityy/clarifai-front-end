aad013c037289505cce2a59187445cdcbfe3459a
Alextricity <alexander.sell@hotmail.com>
2018-07-05 14:49:26 +0100

    failing to compile

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png';
+import logo from './userLogo.png';
 import './Logo.css';
 
 const Logo = () => {
@@ -8,7 +8,7 @@ const Logo = () => {
     <div>
       <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner">
-          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+          <img style={{paddingTop: '5px'}} alt='logo' src={logo}/>
         </div>
       </Tilt>
     </div>
