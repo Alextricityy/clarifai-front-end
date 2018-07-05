8a665927d57e7cf0023104d557895a4c5e7ed637
Alextricity <alexander.sell@hotmail.com>
2018-07-05 12:19:53 +0100

    added files

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -5,9 +5,9 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner pa3">
+    <div>
+      <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner">
           <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
       </Tilt>
