476cb428ecbb01beb34146abc4fe715fe69fdfa9
Alextricity <alexander.sell@hotmail.com>
2018-07-05 12:18:43 +0100

    added files

---
+++ src/components/nav/nav.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if (isSignedIn) {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')}>Sign Out</p>
+        </nav>
+      );
+    } else {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signin')}>Sign In</p>
+          <p onClick={() => onRouteChange('register')}>Register</p>
+        </nav>
+      );
+    }
+}
+
+export default Navigation;
